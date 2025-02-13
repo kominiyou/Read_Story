f522b4b1e905a0b681f0713b5b835aa773cd8650
freechangaming <freechan123z@gmail.com>
2025-02-13 15:19:03 +0000

    Update all files except node_modules

--- MP3_URL/music_url.js
+++ MP3_URL/music_url.js
@@ -3,5 +3,16 @@ export const musicUrls = [
 	"https://drive.google.com/uc?export=download&id=1ajDXW7C58EHvGBwfC8K8KmWCNSLrIbbk",
 	"https://drive.google.com/uc?export=download&id=1pPfZYFUwd_rXqKit5rhwJS2PwHWFRnGS",
 	"https://drive.google.com/uc?export=download&id=1Kspr3wig1uqKttQX4Y48u3nLHxtB_LTw",
-	"https://drive.google.com/uc?export=download&id=1sj8nmDvWjFNoG1PozTKRZkp3GF1Qg3CC"
+	"https://drive.google.com/uc?export=download&id=1sj8nmDvWjFNoG1PozTKRZkp3GF1Qg3CC",
+	"https://drive.google.com/uc?export=download&id=1CQhMkCWQlCZRJcf_72VdHMYh5hK7DCzU",
+	"https://drive.google.com/uc?export=download&id=1tfMfKqwYgaGsJT6lcKpfNxZAIZgtgffx",
+	"https://drive.google.com/uc?export=download&id=19hXXeVQ6GfAjZxDOM9lrlttwLJ_2ypMd",
+	"https://drive.google.com/uc?export=download&id=1pPfZYFUwd_rXqKit5rhwJS2PwHWFRnGS",
+	"https://drive.google.com/uc?export=download&id=1ILGsNRIiyPKI1jVUGk128GM-hJ5RPuXB",
+	"https://drive.google.com/uc?export=download&id=1IyCrfzAt-gZptFWGp2z2unfsIG9IraBN",
+	"https://drive.google.com/uc?export=download&id=11GVe5801C1fmLCn0ZQnJUxcHye5kNphq",
+	"https://drive.google.com/uc?export=download&id=11j4_L0wp3J_g96mMc0RJ_oyyPQeKyiVz",
+    "https://drive.google.com/uc?export=download&id=1QI2Jhizw1TanZaIQ8BLzkrkHNQuJ_RID",
+    "https://drive.google.com/uc?export=download&id=13J3gtf_9KSHXFrmXq0jDVMvZ4nE38tst",
+	"https://drive.google.com/uc?export=download&id=122rKducSVviUz3Z7TyAW3xNcI21AnCCE"
 ];
